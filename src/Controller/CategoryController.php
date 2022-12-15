d0fb3a93b593259298efa746006aa4d721f2f2e7
Ugo DA SILVA <ugodasilva67@gmail.com>
2022-12-15 12:08:47 +0100

    add migration

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -2,19 +2,23 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Category;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class CategoryController extends AbstractController
 {
     /**
      * @Route("/category", name="app_category")
      */
-    public function index(): Response
+    public function index(ManagerRegistry $doctrine): Response
     {
+        // FONCTION QUI RECUPERE TOUT LES categoryS DE LA BDD
+        $categories = $doctrine->getRepository(Category::class)->findAll();
         return $this->render('category/index.html.twig', [
-            'controller_name' => 'CategoryController',
+            'categories' => $categories
         ]);
     }
 }
